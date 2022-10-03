24f87f882016935770283e35252038b7c8e901d9
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-10-03 16:53:26 -0500

    Added basic structure for removing elements to make room for About Us content

--- src/landing.js
+++ src/landing.js
@@ -1,3 +1,18 @@
 export default function landing(){
-    console.log('Clicked on Landing Button!');
+    //console.log('Clicked on Landing Button!');
+    
+    const content = document.querySelector('#content');
+    const header = document.querySelector('.header');
+    while(header.nextSibling){header.nextSibling.remove();}
+
+    const history = document.createElement('div');
+    history.classList.add('info');
+    history.textContent = 'Our Roots are Unknowable';
+
+    const historyExplain = document.createElement('p');
+    historyExplain.classList.add('infoText');
+    historyExplain.textContent = 'Our seed was planted before the universe was. Sown by a being who is unknowable, we have become like them. Children in the truest sense, we cannot maintain any single form.';
+    history.appendChild(historyExplain);
+
+    content.appendChild(history);
 }
