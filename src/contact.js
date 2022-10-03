b07b8a2daf826947b799757f5488d22c7ac3d643
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-10-03 16:51:14 -0500

    Added basic structure for removing elements to make room for contact content

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,18 @@
 export default function contact(){
-    console.log('Clicked for contact info!');
+    //console.log('Clicked for contact info!');
+    
+    const content = document.querySelector('#content');
+    const header = document.querySelector('.header');
+    while(header.nextSibling){header.nextSibling.remove();}
+
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
+    contact.textContent = 'Chance a Connection';
+
+    const phone = document.createElement('div');
+    phone.classList.add('phone');
+    phone.textContent = 'We can be reach by guessing the correct digits in the +[#|##] ### - #### format. Long distance charges will likely apply, but depend on the number when you reach it.';
+    contact.appendChild(phone);
+
+    content.appendChild(contact);
 }
