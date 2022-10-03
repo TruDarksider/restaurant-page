a0aa0a78c1e06ef93bfbb98dead7cac05323e68e
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-10-03 16:52:39 -0500

    Added basic structure for removing elements to make room for menu content

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,18 @@
 export default function menu(){
-    console.log('Clicked on Menu!');
+    //console.log('Clicked on Menu!');
+
+    const content = document.querySelector('#content');
+    const header = document.querySelector('.header');
+    while(header.nextSibling){header.nextSibling.remove();}
+
+    const menu = document.createElement('div');
+    menu.classList.add('menu');
+    menu.textContent = 'Menu of the Moment';
+
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('menuItem');
+    menuItem.textContent = 'Mystery Mug #1: Includes a drink containing notes of a fruit, a nut, and either chocolate or flower.';
+    menu.appendChild(menuItem);
+
+    content.appendChild(menu);
 }
